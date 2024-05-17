8437d4e8397e828e1f226b2f9b22e60bfe621239
Simpl1fy <gourabdas07012004@gmail.com>
2024-05-17 20:04:30 +0530

    added a form for taking input

--- index.php
+++ index.php
@@ -24,6 +24,24 @@
         </div>
     </nav>
 
+    <!-- Adding the main form -->
+
+
+    <div class="container">
+        <form class="mt-3">
+            <h3 class="my-3">Add Note</h3>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Note Heading</label>
+                <input type="text" class="form-control" id="exampleInputheading">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Note Description</label>
+                <input type="text" class="form-control" id="note-desc">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
